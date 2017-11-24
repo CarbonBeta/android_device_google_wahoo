ea44671096e482a2eea2540cadd332b70352a330
Christian Oder <myself5@carbonrom.org>
2017-11-24 18:00:24 +0100

    wahoo: Carbonify
    Signed-off-by: Joe Maples <joe@frap129.org>

--- device.mk
+++ device.mk
@@ -675,3 +675,5 @@ ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
   PRODUCT_PROPERTY_OVERRIDES += \
       persist.vendor.usb.usbradio.config=diag
 endif
+
+-include device/google/wahoo/device-carbon.mk
