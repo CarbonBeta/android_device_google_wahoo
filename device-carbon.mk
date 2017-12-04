2fccad5655e63bdd5110cf48ee9f86109e1ab265
Christian Oder <myself5@carbonrom.org>
2017-12-05 00:10:57 +0100

    wahoo: Add MiFare Permission file
    The NFC Hardware is properly configures to read MiFare chips, so we might as well show the MiFare Apps in the App store
    
    Change-Id: I34a237305ddc9cfaa6173e42e69525a5bf6f2747
    Signed-off-by: Joe Maples <joe@frap129.org>

--- device-carbon.mk
+++ device-carbon.mk
@@ -2,3 +2,7 @@
 
 PRODUCT_PROPERTY_OVERRIDES += \
       media.recorder.show_manufacturer_and_model=true
+
+# MiFare Permissions file
+PRODUCT_COPY_FILES += \
+      frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
