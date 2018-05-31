bc776768c4f31c44c277843d8cca361a2d8a6d9e
Christian Oder <myself5@carbonrom.org>
2018-05-31 16:53:02 +0200

    wahoo: move defconfig definition to wahoo
    * Use commonized defconfig for taimen and walleye
    
    Change-Id: I02d1b3eab684d41b3edf307d86c562211e1d6f43
    Signed-off-by: Joe Maples <joe@frap129.org>

--- BoardConfigCarbon.mk
+++ BoardConfigCarbon.mk
@@ -1,5 +1,6 @@
 # Custom Carbon BoardConfig for wahoo
 # Inline kernel building
+TARGET_KERNEL_CONFIG := carbon_wahoo_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/wahoo
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_KERNEL_CLANG_COMPILE := true
