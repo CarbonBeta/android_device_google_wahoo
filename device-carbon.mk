b80016a781f5b718400d1b225e7f208704d2804b
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-11 03:00:42 -0700

    wahoo: Enable Smart Pixels
    Change-Id: I83e9481ead31e85f2c16579612213a94242deac6

--- device-carbon.mk
+++ device-carbon.mk
@@ -1,5 +1,7 @@
 # Carbon specific carbon device config
 
+DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-carbon
+
 PRODUCT_PROPERTY_OVERRIDES += \
       media.recorder.show_manufacturer_and_model=true
 
